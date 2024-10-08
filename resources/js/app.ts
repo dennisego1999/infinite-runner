463b9291cee67920b4f683256ed8dbcaa6493a26
Dennis Ego <dennis.ego@artcoresociety.com>
2024-10-08 15:50:41 +0200

    Add sass

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,4 +1,4 @@
-import '../css/app.css';
+import '../scss/app.scss';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
@@ -9,14 +9,46 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
-        ),
+    title: (title) => {
+        const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+        if (!title) {
+            return appName;
+        }
+
+        return `${title} - ${appName}`;
+    },
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+        return createApp({
+            created() {
+                router.on('invalid', (event) => {
+                    // Do not prevent the default behavior while developing
+                    if (process.env.NODE_ENV !== 'production') {
+                        return;
+                    }
+
+                    // Do not prevent the handler for server errors or validation checks
+                    if (event.detail.response.status === 500 || event.detail.response.status === 403) {
+                        return;
+                    }
+
+                    // Prevent a white modal
+                    event.preventDefault();
+
+                    // Log the invalid response info
+                    console.error('An invalid Inertia response was received.');
+                    console.error(event.detail.response);
+                });
+            },
+
+            mounted() {
+                // Remove the props to initialize Vue
+                document.getElementById('app').removeAttribute('data-page');
+            },
+
+            render: () => h(App, props)
+        })
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
